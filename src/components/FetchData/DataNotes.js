cf5936d386bdd02429da59d6c4af19216fb7792b
PedroPortow <pedrolportow@gmail.com>
2022-05-18 21:30:22 -0300

    fix cors

--- src/components/FetchData/DataNotes.js
+++ src/components/FetchData/DataNotes.js
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query'
 import { useFetch } from '../../hooks/useFetch'
 
 async function FetchDataNotes() {
-    const cors = 'https://justcors.com/tl_d625c00/'
+    const cors = 'https://justcors.com/tl_ab637e4/'
     const token = '379238b5-705c-48bc-b8c9-27e26676b417'
     const data = await fetch(`${cors}https://farmbox.cc/api/public/technical_visit_report/notes.json?token=${token}`)
 
--- src/components/FetchData/DataPlantation.js
+++ src/components/FetchData/DataPlantation.js
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query'
 import { useFetch } from '../../hooks/useFetch'
 
 async function FetchDataPlantation() {
-    const cors = 'https://justcors.com/tl_d625c00/'
+    const cors = 'https://justcors.com/tl_ab637e4/'
     const token = '379238b5-705c-48bc-b8c9-27e26676b417'
     const data = await fetch(`${cors}https://farmbox.cc/api/public/technical_visit_report/plantations.json?token=${token}`)
     
